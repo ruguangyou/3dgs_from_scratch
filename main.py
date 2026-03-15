98a2752f10018c51113633b682ef35a77d2ad2de
Ruguang You <ruguang.you@outlook.com>
2026-03-15 19:11:16 +0800

    Add ruff as code formater

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from tqdm import tqdm
 from dataset import load_colmap, Dataset
 
 
-logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 def train():
@@ -16,8 +14,7 @@ def train():
     camera_data = load_colmap(data_dir="colmap_data")
 
     train_dataset = Dataset(camera_data, split="train")
-    train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=1, shuffle=True)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True)
     train_dataiter = iter(train_dataloader)
 
     logging.info("Training started...")
